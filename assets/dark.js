581fc12faa60a56cf33407498322b02e3bb00419
ItzTaylorHK <help.nicore@gmail.com>
2019-12-01 22:43:49 +0800

    lte

--- assets/dark.js
+++ assets/dark.js
@@ -6,13 +6,21 @@
 //}
 
 function toggleDarkLight() {
+    $(".popup-overlay, .popup-content").addClass("active");
+};
+
+function yay() {
+    $(".popup-overlay, .popup-content").removeClass("active");
     if ($("body").hasClass("light")) {
         $("body").removeClass("light");
         $("body").addClass("dark");
-        var header = document.getElementById('header');
         $("header").addClass("is-dark");
     } else if ($("body").addClass("dark")) {
         $("body").removeClass("dark");
         $("body").addClass("light");
     }
-};
+}
+
+function nay() {
+    $(".popup-overlay, .popup-content").removeClass("active");
+}
